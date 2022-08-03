dc3265cfc22a44f719838cdd6cda42bb1fe6c1a9
Danreyess <danieljuarezreyes2000@gmail.com>
2022-08-03 11:50:28 -0500

    Conociendo el Hook useState

---
+++ src/components/MiPrimerEstado.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useState } from 'react'
+
+export const MiPrimerEstado = () => {
+
+/*const nombre = "Daniel"
+    const cambiarNombre = e => {
+        nombre = "Daniel Reyes"
+    } */
+
+    const [nombre, setNombre] = useState("Daniel Reyes")
+
+    const cambiarNombre = e => {
+        setNombre("Daniel")
+    }
+    
+  return (
+    <div>
+        <h3>Componente: MiPrimerEstado</h3>
+        <strong>
+            {nombre}
+        </strong>
+        <button onClick = {cambiarNombre}>Cambiar</button>
+    </div>
+  )
+}
